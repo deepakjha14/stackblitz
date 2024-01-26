94db22167f29546944b0319baad52862ac4738c0
Deepak Jha <deepakjha14.ex@gmail.com>
2024-01-26 15:49:33 +0530

    afterRender & afterNextRender method included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,65 @@
-import { Component } from '@angular/core';
-import { CountService } from './shared/count.service';
+import { Component, WritableSignal, signal, HostListener, afterRender, afterNextRender, ElementRef, AfterRenderPhase } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'stackblitz';
+	title = 'Stackblitz addition math test';
+	randomNumber!: number;
+	firstOps!: number;
+	secondNumber!: any;
+	@HostListener('window:keydown.enter', ['$event'])
+	onEnter() {
+		this.checkMaths();
+	}
 
-  constructor(
-    public countService: CountService
-  ) {}
+	gameState: WritableSignal<number> = signal(0);
+
+	constructor(
+		private el: ElementRef
+	) {
+		this.updateNewEquation();
+
+		afterRender(() => {
+			if (this.gameState()>0 && this.gameState()<6) {
+				this.el.nativeElement.querySelector("#progressBar").style.width = `${this.gameState()*20}%`;
+			} else if (this.gameState() > 5 ){
+				this.gameState.update(val => 0);
+				this.el.nativeElement.querySelector("#progressBar").style.width = `0%`;
+			} else if (this.gameState() === 0) {
+				this.el.nativeElement.querySelector("#progressBar").style.width = `0%`;
+			}
+		});
+
+		afterNextRender(() => {
+			const progBar = this.el.nativeElement.querySelector("#progressBar");
+			const progBarResizeObs = new ResizeObserver(
+				(entries) => {
+					console.log(entries[0].contentRect.width, "resize observable");
+				}
+			);
+			progBarResizeObs.observe(progBar);
+		});
+	}
+
+	ngOnInit(): void {
+	}
+
+	updateNewEquation(): void {
+		this.randomNumber = Math.floor(Math.random()*100);
+		this.firstOps = Math.floor(Math.random()*50);
+		this.secondNumber = "";
+	}
+
+	checkMaths(): void {
+		let total = this.secondNumber + this.firstOps;
+		if (total === this.randomNumber ) {
+			this.gameState.update(val => val + 1);
+		} else {
+			this.gameState.update(val => 0);
+		}
+		this.updateNewEquation();
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,9 +12,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule,
-    AppRoutingModule,
-    SharedModule.forRoot()
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
